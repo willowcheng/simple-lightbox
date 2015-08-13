4b497571e91f2521110800e553c3b8416f46680e
willowcheng <e.wingus@Gmail.com>
2015-08-13 14:07:49 -0400

    Perform part 1

--- js/app.js
+++ js/app.js
@@ -5,9 +5,15 @@
 //Solution: Create an overlay with the large image - Lightbox
 
 //1. Capture the click event on a link to an image
+$("#imageGallery a").click(function(event) {
+    event.preventDefault();
+    var href = $(this).attr("href");
+    console.log(href);
     //1.1 Show the overlay.
     //1.2 Update overlay with the image linked in the link
     //1.3 Get child's alt attribute and set caption
+});
+
 //2. Add overlay
     //2.1 An image
     //2.2 A caption
