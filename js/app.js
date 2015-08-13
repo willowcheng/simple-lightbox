1b6d30d4480aec6a6b8235e39e04eb58c068e4e2
willowcheng <e.wingus@Gmail.com>
2015-08-13 14:28:53 -0400

    Perform part 3, click to disappear overlay

--- js/app.js
+++ js/app.js
@@ -5,21 +5,32 @@
 //Solution: Create an overlay with the large image - Lightbox
 
 var $overlay = $('<div id="overlay"></div>');
+var $image = $('<img>');
+
+//Add image to overlay
+$overlay.append($image);
 //Add overlay
 $("body").append($overlay);
-    //An image
+
     //A caption
 
 //Capture the click event on a link to an image
 $("#imageGallery a").click(function(event) {
     event.preventDefault();
-    var href = $(this).attr("href");
-    $overlay.show();
-    //Show the overlay.
+    var imageLocation = $(this).attr("href");
     //Update overlay with the image linked in the link
+    $image.attr("src", imageLocation);
+
+    //Show the overlay.
+    $overlay.show();
+
+
     //Get child's alt attribute and set caption
 });
 
 
 //When overlay is clicked
+$overlay.click(function() {
     //Hide the overlay
+    $overlay.hide();
+});
