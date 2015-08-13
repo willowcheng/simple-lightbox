06cb89075520ea47f054a0d5f91f5503566a5f73
willowcheng <e.wingus@Gmail.com>
2015-08-13 14:38:02 -0400

    Perform part 4, display caption

--- js/app.js
+++ js/app.js
@@ -6,13 +6,17 @@
 
 var $overlay = $('<div id="overlay"></div>');
 var $image = $('<img>');
+var $caption = $('<p></p>');
 
-//Add image to overlay
+//An image to overlay
 $overlay.append($image);
+
+//A caption to overlay
+$overlay.append($caption);
+
 //Add overlay
 $("body").append($overlay);
 
-    //A caption
 
 //Capture the click event on a link to an image
 $("#imageGallery a").click(function(event) {
@@ -24,8 +28,9 @@ $("#imageGallery a").click(function(event) {
     //Show the overlay.
     $overlay.show();
 
-
     //Get child's alt attribute and set caption
+    var captionText = $(this).children("img").attr("alt");
+    $caption.text(captionText);
 });
 
 
